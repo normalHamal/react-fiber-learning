79fa3c4f24a67da1c403fdf916e6f164d9217dce
normalHamal <3292106931@qq.com>
2020-03-03 15:39:58 +0800

    init commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import ExampleApplication from './App';
 
 const start = new Date().getTime();
 
-ReactDOM.render(
-  <ExampleApplication />,
-  document.getElementById('root')
-);
+const root = ReactDOM.unstable_createRoot(document.getElementById('root'));
+
+root.render(<ExampleApplication />);
 
 function update() {
   const elapsed = new Date().getTime() - start;
   const t = (elapsed / 1000) % 10;
   const scale = 1 + (t > 5 ? 10 - t : t) / 10;
-  document.querySelector('#anim').style.transform = 'scaleX(' + (scale / 2.1) + ') scaleY(0.7) translateZ(0.1px)';
+  const warp = document.querySelector('#anim');
+  warp && (document.querySelector('#anim').style.transform = 'scaleX(' + (scale / 2.1) + ') scaleY(0.7) translateZ(0.1px)');
   requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
